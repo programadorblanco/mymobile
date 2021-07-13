5a56bdf2b84e8c1a44a2f56c497a5d6c0e79ff43
Jesus <jesus.ramirezg17@tectijuana.edu.mx>
2021-07-13 02:22:28 -0500

    Terminos y Condiciones

--- termsConditions.php
+++ termsConditions.php
@@ -6,71 +6,78 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="../css/styles.css">
-        <script src="../js/jquery.js"></script>
-        <script src="../js/main.js"></script>
-        <script src="../js/html2pdf.js"></script>
-        <script src="../js/convertPDF.js"></script>
+        <link rel="stylesheet" href="css/terms.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/main.js"></script>
         <title>My Mobile LLC</title>
     </head>
     <body>
         <div class="container-form">
+            <div class="box">
+                <img src="img/logo.jpg" class="logo">
+            </div>
             <div class="flex">
+                <div class="texttB">
+                    <label><hr>Authorization<hr><label>
                 <div>
-                    <hr>
-                    <label>Authorization<label><hr>
-                    <label>
-                        Authorization: By signing below, you authorize: (a) backgroundchecks.com (“BGC”) to request
-                        information about you from any public or private information source; (b) anyone to provide information
-                        about you to BGC; (c) BGC to provide us My Mobile, LLC and its client, TracFone Wireless, Inc., one 
-                        or more reports based on that information; and (d) us to share those reports with others for legitimate
-                        business purposes related to your employment. BGC may investigate your education, work history,
-                        professional licenses and credentials, references, address history, social security number validity,
-                        right to work, criminal record, lawsuits, driving record, credit history, and any other information
-                        with public or private information sources. You acknowledge that a fax, image, or copy of this
-                        authorization is as valid as the original. You make this authorization to be valid for as long as you 
-                    </label>
-                    <label>
+                <div class="textt">
+                    <div><label>
+                        Authorization: By signing below, you authorize: (a) backgroundchecks.com (“BGC”) to request information
+                        about you from any public or private information source; (b) anyone to provide information about you to
+                        BGC; (c) BGC to provide us My Mobile, LLC and its client, TracFone Wireless, Inc., one or more reports
+                        based on that information; and (d) us to share those reports with others for legitimate business
+                        purposes related to your employment. BGC may investigate your education, work history, professional
+                        licenses and credentials, references, address history, social security number validity, right to work,
+                        criminal record, lawsuits, driving record, credit history, and any other information with public
+                        or private information sources. You acknowledge that a fax, image, or copy of this authorization
+                        is as valid as the original. You make this authorization to be valid for as long as you are an
+                        applicant or employee with us.
+                    </label></div>
+                    <br>
+                    <div><label>
                         The Consumer Financial Protection Bureau’s “Summary of Your Rights under the Fair Credit Reporting Act”
                         is attached to this authorization. If you are a New York applicant, a copy of New York’s law on the
                         use of criminal records is attached. By signing below, you acknowledge receipt of these documents.
-                    </label>
-                    <hr>
-                    <label>Disclosure<label><hr>
-                    <label>
+                    </label></div>
+                </div>
+                <div class="texttB">
+                    <div><label><hr>Disclosure<hr><label></div>
+                <div>
+                <div class="textt">
+                    <div><label>
                         We My Mobile, LLC and our client, TracFone Wireless, Inc., will obtain one or more consumer reports
                         or investigative consumer reports (or both) about you for purposes of employment with My Mobile,
                         LLC and potential assignment to work on projects for our client TracFone Wireless, Inc. These 
                         purposes may include hiring, contract, assignment, promotion, re-assignment, and termination. The 
                         reports will include information about your character, general reputation, personal characteristics,
                         and mode of living.
-                    </label>
+                    </label></div>
                     <br>
-                    <label>
+                    <div><label>
                         We will obtain these reports through a consumer reporting agency. Our consumer reporting agency is 
                         backgroundchecks.com (“BGC”). BGC’s address is P.O. Box 353, Chapin, SC 29036. BGC’s telephone number 
                         is (866) 265-6602. BGC’s website is www.backgroundchecks.com, where you can find information about 
                         whether BGC’s international privacy practices.
-                    </label>
+                    </label></div>
                     <br>
-                    <label>
+                    <div><label>
                         To prepare the reports, BGC may investigate your education, work history, professional licenses and 
                         credentials, references, address history, social security number validity, right to work, criminal 
                         record, lawsuits, driving record, credit history, and any other information with public or private 
                         information sources.
-                    </label>
+                    </label></div>
                     <br>
-                    <label>
+                    <div><label>
                         You may obtain a copy of any report that BGC provides and BGC’s files about you (in person, by mail, 
                         or by phone) by providing identification to BGC. If you do, BGC will provide you help to understand 
                         the files, including trained personnel and an explanation of any codes. Another person may accompany 
                         you by providing identification.
-                    </label>
+                    </label></div>
                     <br>
-                    <label>
+                    <div><label>
                         If BGC obtains any information by interview, you have the right to obtain a complete and accurate 
                         disclosure of the scope and nature of the investigation performed.
-                    </label>
+                    </label></div>
                 </div>
             </div>
         </div> 
